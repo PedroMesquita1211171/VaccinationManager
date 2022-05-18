43a1d1a849ffd632397773e02cbec2919c51c389
Henrique <1211201@isep.ipp.pt>
2022-05-18 22:09:12 +0100

    US14 Programming Ending. Design update needed. Added confirmation blocks in the UI.

--- src/main/java/app/ui/console/LoadSNSUsersUI.java
+++ src/main/java/app/ui/console/LoadSNSUsersUI.java
@@ -18,37 +18,69 @@ public class LoadSNSUsersUI implements Runnable {
     public void run() {
 
         try{
-            String option;
+            String option, confirmation1, confirmation2;
 
             do{
-                option = Utils.readLineFromConsole("What type of file do you want to load?\n1 - “;” separated(with header)\n2 - “,” separated\n");
 
-                if(!(option.equals("1")||option.equals("2"))){
-                    System.out.println("Invalid option.\nTry again.");
-                    option = "invalid";
-                }
+                do{
 
-            }while(option.equals("invalid"));
+                    option = Utils.readLineFromConsole("What type of file do you want to load?\n1 - “;” separated(with header)\n2 - “,” separated\n");
 
-            String fileName = Utils.readLineFromConsole("Enter the file name/path: ");
+                    if(!(option.equals("1")||option.equals("2"))){
+                        System.out.println("\nInvalid option.\nTry again.");
+                        option = "invalid";
+                    }
 
-            File file = new File(fileName);
-            String regex = "";
 
-            if(file.exists()){
-                if(option.equals("1")){
-                     regex = ";";
-                    this.loadSNSUserFromFile.LoadOption(file, regex);
+                  }while(option.equals("invalid"));
 
-                }else if (option.equals("2")){
-                     regex = ",";
-                    this.loadSNSUserFromFile.LoadOption(file, regex);
+                    confirmation1 = Utils.readLineFromConsole("Is this the correct file type?(y/n)\n");
 
-                }
-            }
-            else{
-                System.out.println("File does not exist\nReturning to main menu.");
-            }
+                    if(!(confirmation1.equalsIgnoreCase("y")||confirmation1.equalsIgnoreCase("yes")||confirmation1.equalsIgnoreCase("n")||confirmation1.equalsIgnoreCase("no"))){
+                        throw new IllegalArgumentException("\nInvalid option in yes or no block.\nReturning to main menu.");
+                    }
+
+            }while(!(confirmation1.equalsIgnoreCase("y")||confirmation1.equalsIgnoreCase("yes")));
+
+
+
+             do{
+
+                 String fileName = Utils.readLineFromConsole("Enter the file name/path: ");
+
+                 confirmation2 = Utils.readLineFromConsole("Is this the correct file type?(y/n)\n");
+
+                 if(!(confirmation2.equalsIgnoreCase("y")||confirmation2.equalsIgnoreCase("yes")||confirmation2.equalsIgnoreCase("n")||confirmation2.equalsIgnoreCase("no"))){
+                     throw new IllegalArgumentException("\nInvalid option in yes or no block.\nReturning to main menu.");
+                 }
+
+                 if(confirmation2.equalsIgnoreCase("y")||confirmation2.equalsIgnoreCase("yes")){
+
+                     File file = new File(fileName);
+                     String regex = "";
+
+                     if(file.exists()){
+                         if(option.equals("1")){
+                             regex = ";";
+                             this.loadSNSUserFromFile.LoadOption(file, regex);
+
+                         }else if (option.equals("2")){
+                             regex = ",";
+                             this.loadSNSUserFromFile.LoadOption(file, regex);
+
+                         }
+                     }
+                     else{
+                         System.out.println("\nFile does not exist\nReturning to main menu.");
+                     }
+
+
+                 }
+
+
+               }while(!(confirmation2.equalsIgnoreCase("y")||confirmation2.equalsIgnoreCase("yes")));
+
+
         }catch(IllegalArgumentException e){
             System.out.println(e.getMessage());
         }
